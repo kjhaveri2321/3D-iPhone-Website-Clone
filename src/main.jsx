820d4ea3126a9d549ca92435733bf557be56b8e8
kjhaveri2321 <116605701+kjhaveri2321@users.noreply.github.com>
2024-07-23 19:19:07 -0600

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://65673a81d30ac12309d1091fc8fc18fe@o4507626830495744.ingest.us.sentry.io/4507626837245952",
+  
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
